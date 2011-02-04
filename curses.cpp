45cf1d2804ca574a069b6e2b797618d9f063b02a
Aaron Faanes <dafrito@gmail.com>
2011-02-03 20:39:37 -0600

    A simple, animated fake-typing program

--- curses.cpp
+++ curses.cpp
@@ -1,11 +1,28 @@
+#include <ctime>
+#include <string>
+#include <random>
 #include <ncurses.h>
+
+using namespace std;
+
+mt19937 eng(time(NULL));
+normal_distribution<double> unif(100, 50);
+variate_generator<mt19937, normal_distribution<double> > randn(eng, unif);
+
+void fake_type(const string& text)
+{
+	string::const_iterator it;
+	for (it = text.begin(); it != text.end(); it++) {
+		addch(*it);
+		refresh();
+		napms(randn());
+	}
+}
 
 int main(void)
 {
 	initscr();
-	addstr("Goodbye, cruel C programming!");
-	refresh();
-
+	fake_type("Greetings from NCurses!\n");
 	endwin();
 	return 0;
 }
