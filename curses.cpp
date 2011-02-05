07dacf43742f88bc563e2e219cf177e2b4b2ca77
Aaron Faanes <dafrito@gmail.com>
2011-02-04 18:31:22 -0600

    Pretty 4-mirrored window program

--- curses.cpp
+++ curses.cpp
@@ -17,41 +17,57 @@ int main(void)
 	if (!has_colors() || start_color() != OK)
 		die("Terminal failed to start colors");
 
+	WINDOW *tl, *tr, *bl, *br;
 
-	WINDOW *help;
-	if ((help = newwin(0,0,0,0)) == NULL)
-		die("Couldn't create new window");
-	
-	mvwaddstr(help, 0, 0, "Looking for help?");
-	mvwaddstr(help, 2, 0, "It sadly does not reside here.");
-	mvwaddstr(help, 4, 0, "Perhaps elsewhere!");
-	mvwaddstr(help, 6, 0, "Press enter to go back");
+	int maxl, maxc, halfl, halfc;
+	getmaxyx(stdscr, maxl, maxc);
+	halfl = maxl / 2;
+	halfc = maxc / 2;
 
-	addstr("Typer Program\n");
-	addstr("Press + for help!\n\n");
+	const char* errmsg = "Couldn't create new window";
+	// We use (max - half) since an odd number of lines or columns will result in a visible gap.
+	if ((tl = newwin(halfl,        halfc,        0,     0    )) == NULL) die(errmsg);
+	if ((tr = newwin(halfl,        maxc - halfc, 0,     halfc)) == NULL) die(errmsg);
+	if ((bl = newwin(maxl - halfl, halfc,        halfl, 0    )) == NULL) die(errmsg);
+	if ((br = newwin(maxl - halfl, maxc - halfc, halfl, halfc)) == NULL) die(errmsg);
+
+	init_pair(1, COLOR_BLACK, COLOR_BLUE);
+	init_pair(2, COLOR_YELLOW, COLOR_RED);
+	init_pair(3, COLOR_BLACK, COLOR_GREEN);
+	init_pair(4, COLOR_WHITE, COLOR_CYAN);
+
+	mvwaddstr(tl, 0, 0, "Topleft\n");
+	mvwaddstr(tr, 0, 0, "Topright\n");
+	mvwaddstr(bl, 0, 0, "Bottomleft\n");
+	mvwaddstr(br, 0, 0, "Bottomright\n");
+
+	wbkgd(tl, COLOR_PAIR(1));
+	wbkgd(tr, COLOR_PAIR(2) | A_BOLD);
+	wbkgd(bl, COLOR_PAIR(3));
+	wbkgd(br, COLOR_PAIR(4) | A_BOLD);
+
 	refresh();
+	wrefresh(tl);
+	wrefresh(tr);
+	wrefresh(bl);
+	wrefresh(br);
 
-	keypad(stdscr, TRUE);
 	noecho();
+	int ch;
+	while (true) {
+		ch = getch();
+		if (ch == '~')
+			break;
 
-	while(true) {
-		int ch = getch();
-		refresh();
-		switch (ch) {
-			case '+':
-				touchwin(help);
-				wrefresh(help);
-				getch();
-				touchwin(stdscr);
-				refresh();
-				break;
-			case '~':
-				endwin();
-				exit(0);
-				break;
-			default:
-				addch(ch);
-		}
+		waddch(tl, ch);
+		waddch(tr, ch);
+		waddch(bl, ch);
+		waddch(br, ch);
+
+		wrefresh(tl);
+		wrefresh(tr);
+		wrefresh(bl);
+		wrefresh(br);
 	}
 	endwin();
 	return 0;
@@ -59,7 +75,8 @@ int main(void)
 
 void die(const string& v)
 {
+	addstr(v.c_str());
+	refresh();
 	endwin();
-	std::cerr << v << std::endl;
 	exit(1);
 }
