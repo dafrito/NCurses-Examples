572ef33a0d69c2cb42a479fb7fe559037dc6cf4d
Aaron Faanes <dafrito@gmail.com>
2011-02-04 23:21:59 -0600

    A binary clock

--- curses.cpp
+++ curses.cpp
@@ -14,35 +14,51 @@ class BinaryNumber
 {
 private:
 	WINDOW* const window;
-
+	int value;
+public:
 	static const unsigned int COLS=2;
 	static const unsigned int LINES=4;
-public:
+	static const unsigned int AREA=BinaryNumber::COLS * BinaryNumber::LINES;
+
 	BinaryNumber(const int& x, const int& y) : 
-		window(newwin(BinaryNumber::LINES, BinaryNumber::COLS, x, y))
+		window(newwin(BinaryNumber::LINES, BinaryNumber::COLS, x, y)),
+		value(-1)
 	{
 		if(this->window == NULL)
 			die("Window failed to initialize");
 		wbkgd(this->window, '*');
 	}
 
-	void Set(const int& number);
+	void SetValue(const int& number);
+	const int& GetValue();
+
 	~BinaryNumber() {
 		delwin(this->window);
 	}
 };
 
-void BinaryNumber::Set(const int& number)
+void BinaryNumber::SetValue(const int& number)
 {
-	mvwchgat(this->window, 3, 1, 1, (number & 1 << 0 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 2, 1, 1, (number & 1 << 1 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 1, 1, 1, (number & 1 << 2 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 0, 1, 1, (number & 1 << 3 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 3, 0, 1, (number & 1 << 4 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 2, 0, 1, (number & 1 << 5 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 1, 0, 1, (number & 1 << 6 ? A_BOLD : 0), 0, 0);
-	mvwchgat(this->window, 0, 0, 1, (number & 1 << 7 ? A_BOLD : 0), 0, 0);
+	if (this->value == number)
+		return;
+	this->value = number;
+	for (unsigned int i = 0; i < BinaryNumber::AREA; i++) {
+		bool shouldBold = number & 1 << (BinaryNumber::AREA - i - 1);
+		mvwchgat(this->window, 
+			i % 4,
+			i / BinaryNumber::LINES,
+			1,
+			A_BOLD,
+			shouldBold ? 2 : 1,
+			0
+		);
+	}
 	wrefresh(this->window);
+}
+
+const int& BinaryNumber::GetValue()
+{
+	return this->value;
 }
 
 int main(void)
@@ -52,13 +68,45 @@ int main(void)
 	if (!has_colors() || start_color() != OK)
 		die("Terminal failed to start colors");
 
+	init_pair(1, COLOR_BLACK, COLOR_BLACK);
+	init_pair(2, COLOR_RED,   COLOR_BLACK);
+	init_pair(3, COLOR_BLUE,   COLOR_BLACK);
+
 	curs_set(0);
+	nodelay(stdscr, TRUE);
+	noecho();
 
-	BinaryNumber num(0,0);
-	for (int i=0; i < 60; i++) {
-		num.Set(i);
-		napms(1000);
+	const int originl = 1;
+	const int originc = 3;
+	const int spacing = 3;
+	BinaryNumber hours  (originl, originc);
+	BinaryNumber minutes(originl, originc + BinaryNumber::COLS + spacing);
+	BinaryNumber seconds(originl, originc + (BinaryNumber::COLS + spacing) * 2);
+	WINDOW *timeString = newwin(1, 20, originl + BinaryNumber::LINES + 1, originc);
 
+	time_t oldTime=0;
+	while(getch() == ERR) {
+		time_t newTime;
+		tm* timeInfo;
+		time(&newTime);
+		timeInfo = localtime(&newTime);
+		if (newTime > oldTime) {
+			seconds.SetValue(timeInfo->tm_sec);
+			minutes.SetValue(timeInfo->tm_min);
+			hours.SetValue(timeInfo->tm_hour);
+
+			wattr_on(timeString, COLOR_PAIR(3) | A_BOLD, 0);
+			mvwprintw(timeString, 0, 0, "%02d : %02d : %02d", 
+				hours.GetValue(),
+				minutes.GetValue(),
+				seconds.GetValue()
+			);
+			mvwchgat(timeString, 0, 3, 1, A_NORMAL, 3, 0);
+			mvwchgat(timeString, 0, 8, 1, A_NORMAL, 3, 0);
+			wrefresh(timeString);
+			oldTime=newTime;
+		}
+		napms(100);
 	}
 	endwin();
 	return 0;
